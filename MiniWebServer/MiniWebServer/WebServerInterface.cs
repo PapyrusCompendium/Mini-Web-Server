69b0318b7cdc8b31c87165290d373540997fad04
papyruscompendium <papyruscompendium@protonmail.com>
2019-07-20 18:10:08 -0400

    Added windows forms project & solution file.

---
+++ MiniWebServer/MiniWebServer/WebServerInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniWebServer
+{
+    public partial class WebServerInterface : Form
+    {
+        public WebServerInterface()
+        {
+            InitializeComponent();
+        }
+    }
+}
